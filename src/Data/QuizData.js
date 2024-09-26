4bfe47afc2af573f489f1940fb83fe8c605c728c
alisami0111 <saminagri0111@gmail.com>
2024-09-26 18:26:35 +0500

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question: "Which company developed the React library?",
+        options: ["Google", "Facebook", "Twitter", "Microsoft"],
+        answer: 2
+    },
+    {
+        question: "Which HTML tag is used to define an unordered list?",
+        options: ["<ul>", "<li>", "<ol>", "<list>"],
+        answer: 1
+    },
+    {
+        question: "What does SQL stand for?",
+        options: ["Structured Query Language", "Stylized Query Language", "Simple Query Language", "Standard Query Language"],
+        answer: 1
+    },
+    {
+        question: "Which protocol is used to send web pages?",
+        options: ["SMTP", "FTP", "HTTP", "TCP"],
+        answer: 3
+    },
+    {
+        question: "Which of the following is not a JavaScript data type?",
+        options: ["Number", "String", "Boolean", "Class"],
+        answer: 4
+    },
+    {
+        question: "What is the default port for HTTP?",
+        options: ["80", "8080", "443", "21"],
+        answer: 1
+    },
+    {
+        question: "Which version of HTML introduced semantic elements?",
+        options: ["HTML4", "HTML5", "XHTML", "None of the above"],
+        answer: 2
+    },
+    {
+        question: "What is the purpose of the 'this' keyword in JavaScript?",
+        options: ["Refers to the current object", "Refers to the global object", "Refers to the previous object", "Refers to a new object"],
+        answer: 1
+    },
+    {
+        question: "Which symbol is used for comments in CSS?",
+        options: ["//", "<!-- -->", "/* */", "#"],
+        answer: 3
+    },
+    {
+        question: "Which of the following is a JavaScript framework?",
+        options: ["Laravel", "Django", "React", "Spring"],
+        answer: 3
+    },    
+];
